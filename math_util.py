8e37c2651b20bde28165ac67621fb853db7e67d0
sedromun <vanart2004@mail.ru>
2024-04-16 00:31:16 +0300

    update math_util

--- math_util.py
+++ math_util.py
@@ -13,4 +13,16 @@ def gradient(vector, f):
     x, y = vector
     return derivative_x(x, y, f), derivative_y(x, y, f)
 
+
+def second_derivative_x(x, y, f):
+    return (f(2 * epsilon + x, y) -
+            2 * f(epsilon + x, y) +
+            f(x, y)) / (epsilon * epsilon)
+
+
+def second_derivative_y(x, y, f):
+    return (f(x, y + 2 * epsilon) -
+            2 * f(x, y + epsilon) +
+            f(x, y)) / (epsilon * epsilon)
+
 # def jacobian(self, vector):
